0fd7b62b942686b123a68c039432abe11f712750
xiaozou <1424113865@qq.com>
2022-02-24 09:59:40 +0800

    deploy

---
+++ precache-manifest.3ff8058a94457c468c29b7ad8d7f3a48.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60ab5c23984e13bc9a8",
+    "url": "/xuexue-bookkeeping/css/app.ca6b09e6.css"
+  },
+  {
+    "revision": "8cedd22f7c9000cf3db9",
+    "url": "/xuexue-bookkeeping/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/xuexue-bookkeeping/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/xuexue-bookkeeping/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "692446993b68f9576853b1c2baceaf26",
+    "url": "/xuexue-bookkeeping/img/1.69244699.jpeg"
+  },
+  {
+    "revision": "bc8aebf904124b298c0347dc69c3e21d",
+    "url": "/xuexue-bookkeeping/img/2.bc8aebf9.jpeg"
+  },
+  {
+    "revision": "243e9cd5f47c85f3189167db4c4820e1",
+    "url": "/xuexue-bookkeeping/img/3.243e9cd5.jpeg"
+  },
+  {
+    "revision": "3c58949c9515c6b45cb101eefbb6e004",
+    "url": "/xuexue-bookkeeping/img/avatar.3c58949c.jpg"
+  },
+  {
+    "revision": "6adaa44b58e1cd689d5f955d13395b95",
+    "url": "/xuexue-bookkeeping/img/github1.6adaa44b.png"
+  },
+  {
+    "revision": "3de6a1fba45e189df424bf64022acb8c",
+    "url": "/xuexue-bookkeeping/img/touxiang.3de6a1fb.png"
+  },
+  {
+    "revision": "d64aa9c3b242ff3ed1fecec3dde706c2",
+    "url": "/xuexue-bookkeeping/img/基金训练营.d64aa9c3.jpg"
+  },
+  {
+    "revision": "940d1127e024bbb4f99b2b15bda8d235",
+    "url": "/xuexue-bookkeeping/img/基金高阶训练营.940d1127.jpg"
+  },
+  {
+    "revision": "d1738bdce283d5160c66bef82f9be1ba",
+    "url": "/xuexue-bookkeeping/img/学基础保险课.d1738bdc.jpg"
+  },
+  {
+    "revision": "96a9a62695025d1a45ecca3ba8a73c9d",
+    "url": "/xuexue-bookkeeping/img/小白理财.96a9a626.jpg"
+  },
+  {
+    "revision": "631dcc0911907153424eafb4b2d09d4e",
+    "url": "/xuexue-bookkeeping/img/标签.631dcc09.png"
+  },
+  {
+    "revision": "f168ba8e20a2e210cd322115b2addb53",
+    "url": "/xuexue-bookkeeping/img/理财.f168ba8e.png"
+  },
+  {
+    "revision": "f80def50397a8ddfa77c6dcad47fd45b",
+    "url": "/xuexue-bookkeeping/img/知乎.f80def50.png"
+  },
+  {
+    "revision": "afbf8cdfe04f0f42b21784771ab66148",
+    "url": "/xuexue-bookkeeping/img/股票训练营.afbf8cdf.jpg"
+  },
+  {
+    "revision": "7c5b3d12a490826f1a0cd83051eeead3",
+    "url": "/xuexue-bookkeeping/img/账本.7c5b3d12.png"
+  },
+  {
+    "revision": "cbe4bd4d869d9af1699b17580818b0cd",
+    "url": "/xuexue-bookkeeping/img/货币基金捡钱课.cbe4bd4d.jpg"
+  },
+  {
+    "revision": "a107914484886bb3c6da528cddf2010e",
+    "url": "/xuexue-bookkeeping/index.html"
+  },
+  {
+    "revision": "a60ab5c23984e13bc9a8",
+    "url": "/xuexue-bookkeeping/js/app.326fb33d.js"
+  },
+  {
+    "revision": "8cedd22f7c9000cf3db9",
+    "url": "/xuexue-bookkeeping/js/chunk-vendors.3555b02f.js"
+  },
+  {
+    "revision": "dabf655311b1408d2798f4f58c7fca7e",
+    "url": "/xuexue-bookkeeping/manifest.json"
+  },
+  {
+    "revision": "278e32771c83a6f6c0d6720edf1961fd",
+    "url": "/xuexue-bookkeeping/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xuexue-bookkeeping/robots.txt"
+  }
+]);
